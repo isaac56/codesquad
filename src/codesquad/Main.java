f5c8433dcadf6637911e75ce8b72d4c0cd453fe9
정이삭 <isaac56@naver.com>
2020-12-09 22:02:52 +0900

    Make main method
    커맨드를 입력받는 메인 메소드 생성

--- src/codesquad/Main.java
+++ src/codesquad/Main.java
@@ -1,9 +1,26 @@
 package codesquad;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Cube cube = new Cube();
+		System.out.println(cube);
+		
+		while(true) {
+			System.out.print("CUBE> ");
+			String cmds = sc.nextLine();
+			if(cmds.equals("Q")) {
+				System.out.println("Bye~");
+				break;
+			}
+			else {
+				cube.executeCmds(cmds);
+			}
+		}
+		sc.close();
 	}
 
 }
