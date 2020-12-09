a8403d61fef547b683f79bc360b608a8772f3f62
정이삭 <isaac56@naver.com>
2020-12-09 20:27:46 +0900

    Make basic cube's moving method
    큐브의 기본적인 이동 함수 작성
    (왼쪽, 오른쪽, 위, 아래로 이동)

--- src/codesquad/Cube.java
+++ src/codesquad/Cube.java
@@ -6,6 +6,66 @@ public class Cube {
 	public Cube () {
 	}
 	
+	public void shiftLeft(int row) {
+		if(row < 0 || row >= 3)
+			return;
+		
+		char[] temp = new char[3];
+		for(int i = 0; i < 3; i++) {
+			temp[i] = this.current[row][(i + 1) % 3];
+		}
+		replaceRow(row, temp);
+	}
+	
+	public void shiftRight(int row) {
+		if(row < 0 || row >= 3)
+			return;
+		
+		char[] temp = new char[3];
+		for(int i = 0; i < 3; i++) {
+			temp[i] = this.current[row][(3 + i - 1) % 3];
+		}
+		replaceRow(row, temp);
+	}
+	
+	public void shiftUp(int col) {
+		if(col < 0 || col >= 3)
+			return;
+		
+		char[] temp = new char[3];
+		for(int i = 0; i < 3; i++) {
+			temp[i] = this.current[(i + 1) % 3][col];
+		}
+		replaceColumn(col, temp);
+	}
+	
+	public void shiftDown(int col) {
+		if(col < 0 || col >= 3)
+			return;
+		
+		char[] temp = new char[3];
+		for(int i = 0; i < 3; i++) {
+			temp[i] = this.current[(3 + i - 1) % 3][col];
+		}
+		replaceColumn(col, temp);
+	}
+	
+	public void replaceColumn(int col, char[] toReplace) {
+		if(toReplace.length < 3 || (col < 0 || col >= 3)) 
+			return;
+		
+		for(int i = 0; i < 3; i++) {
+			this.current[i][col] = toReplace[i];
+		}
+	}
+	
+	public void replaceRow(int row, char[] toReplace) {
+		if(toReplace.length < 3 || (row < 0 || row >= 3)) 
+			return;
+		
+		this.current[row] = toReplace;	
+	}
+	
 	@Override
 	public String toString() {
 		String result = "";
