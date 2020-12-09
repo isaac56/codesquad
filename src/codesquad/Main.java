6df6e30445299b8b095824d7ff7662221f986021
정이삭 <isaac56@naver.com>
2020-12-09 19:13:18 +0900

    Simplyfy shifting word code
    단어 밀어내기 코드 단순화

--- src/codesquad/Main.java
+++ src/codesquad/Main.java
@@ -11,21 +11,15 @@ public class Main {
 		int cnt = sc.nextInt() % word.length();
 		char cmd = sc.next().charAt(0);
 		
-		if(cnt < 0)
-		{
-			cnt = -cnt;
-			if(cmd == 'L' || cmd == 'l')
-				cmd = 'R';
-			else if (cmd == 'R' || cmd == 'r')
-				cmd = 'L';
-		}
-		
 		if (cmd == 'L' || cmd == 'l') {
-			System.out.println(word.substring(cnt) + word.substring(0, cnt));
+			int startIdx = (word.length() + cnt) % word.length(); 
+			System.out.println(word.substring(startIdx) + word.substring(0, startIdx));
 		}
 		else if (cmd == 'R' || cmd == 'r') {
-			System.out.println(word.substring(word.length() - cnt) + word.substring(0, word.length() - cnt));
+			int startIdx = (word.length() - cnt) % word.length();
+			System.out.println(word.substring(startIdx) + word.substring(0, startIdx));
 		}
+		sc.close();
 	}
 
 }
