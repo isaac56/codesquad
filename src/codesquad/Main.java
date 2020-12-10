01553e4edaedf771d92e1c3c4f774466c433d0bb
정이삭 <isaac56@naver.com>
2020-12-10 20:33:55 +0900

    Implement main method
    Random으로 시작할 지 결정한 후 Q 입력할 때 까지 명령을 내리는 main 메소드 구현

--- src/codesquad/Main.java
+++ src/codesquad/Main.java
@@ -1,10 +1,32 @@
 package codesquad;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		RubiksCube cube;
+		System.out.println("Welcome to Rubik's Cube!");
+		while(true) {
+			System.out.print("Do you want a random cube? (Y / N): ");
+			String input = sc.nextLine();
+			if(input.toUpperCase().equals("Y")) {
+				cube = new RubiksCube(100);
+				break;
+			}
+			else if (input.toUpperCase().equals("N")) {
+				cube = new RubiksCube();
+				break;
+			}
+		}
+		System.out.println(cube);
+		
+		while(true) {
+			System.out.print("Cube> ");
+			if(cube.executeCmds(sc.nextLine()) == false)
+				break;
+		}
+		sc.close();
 	}
-
 }
