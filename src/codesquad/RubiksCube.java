127209fd11495f60790d3da0efc9c02ea6e1cc8f
정이삭 <isaac56@naver.com>
2020-12-10 22:06:50 +0900

    Delete indent in isAllSame method

--- src/codesquad/RubiksCube.java
+++ src/codesquad/RubiksCube.java
@@ -318,8 +318,7 @@ public class RubiksCube {
 			char rep = this.state[0][0];
 			for(int i = 0; i < 3; i++) {
 				for(int j = 0; j < 3; j++) {
-					if (state[i][j] != rep)
-						return false;
+					if (state[i][j] != rep) return false;
 				}
 			}
 			return true;
