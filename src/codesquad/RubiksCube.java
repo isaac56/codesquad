b6676fa93976adc5c7cfd1811bc08725bd1c9415
정이삭 <isaac56@naver.com>
2020-12-10 13:00:26 +0900

    Make Plane inner class into RubiksCube
    큐브의 한 면을 의미하는 Plane 내부클래스 생성

--- src/codesquad/RubiksCube.java
+++ src/codesquad/RubiksCube.java
@@ -1,5 +1,39 @@
 package codesquad;
 
 public class RubiksCube {
-
+	private Plane frontPlane = new Plane('W');
+	private Plane rearPlane = new Plane('G');
+	private Plane leftPlane = new Plane('O');
+	private Plane rightPlane = new Plane('Y');
+	private Plane topPlane = new Plane('B');
+	private Plane bottomPlane = new Plane('R');
+	
+	public RubiksCube() {
+		
+	}
+	
+	private class Plane {
+		private char[][] state;
+		
+		public Plane (char init) {
+			state = new char[3][3];
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					state[i][j] = init;
+				}
+			}
+		}
+		
+		@Override
+		public String toString() {
+			String result = "";
+			for(char[] row : state) {
+				for(char element : row) {
+					result += String.valueOf(element) + " ";
+				}
+				result += "\n";
+			}
+			return result;
+		}
+	}
 }
