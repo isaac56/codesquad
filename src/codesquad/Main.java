01126f780587556520943e47bf9156d246e2a691
정이삭 <isaac56@naver.com>
2020-12-10 21:19:32 +0900

    Seperate asking random from main method
    랜덤 큐브를 생성할지 물어보는 부분 checkRandom 메소드로 분리

--- src/codesquad/Main.java
+++ src/codesquad/Main.java
@@ -7,19 +7,12 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		RubiksCube cube;
+		
 		System.out.println("Welcome to Rubik's Cube!");
-		while(true) {
-			System.out.print("Do you want a random cube? (Y / N): ");
-			String input = sc.nextLine();
-			if(input.toUpperCase().equals("Y")) {
-				cube = new RubiksCube(100);
-				break;
-			}
-			else if (input.toUpperCase().equals("N")) {
-				cube = new RubiksCube();
-				break;
-			}
-		}
+		if(checkRandom(sc)) 
+			cube = new RubiksCube(100);
+		else 
+			cube = new RubiksCube();
 		System.out.println(cube);
 		
 		while(true) {
@@ -27,6 +20,20 @@ public class Main {
 			if(cube.executeCmds(sc.nextLine()) == false)
 				break;
 		}
+		
 		sc.close();
+	}
+	
+	public static Boolean checkRandom(Scanner sc) {
+		while(true) {
+			System.out.print("Do you want a random cube? (Y / N): ");
+			String input = sc.nextLine();
+			if(input.toUpperCase().equals("Y")) {
+				return true;
+			}
+			else if (input.toUpperCase().equals("N")) {
+				return false;
+			}
+		}
 	}
 }
