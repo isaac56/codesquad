4653fb43a9383f20bd6ca6229e38070d1bb82234
정이삭 <isaac56@naver.com>
2020-12-16 01:11:44 +0900

    Add rotate method to Plane inner class
    과제 구현에서 실수로 뺐던 면 자체를 90도 회전하는 함수 추가

--- src/codesquad/RubiksCube.java
+++ src/codesquad/RubiksCube.java
@@ -324,6 +324,28 @@ public class RubiksCube {
 			return true;
 		}
 		
+		public void RotateClockwise() {
+			char temp[][] = new char[3][3];
+			
+			for(int i = 0; i < 3; i++) {
+				for(int j = 0; j < 3; j++) {
+					temp[j][2-i] = this.state[i][j];
+				}
+			}
+			this.state = temp;
+		}
+		
+		public void RotateAntiClockwise() {
+			char temp[][] = new char[3][3];
+			
+			for(int i = 0; i < 3; i++) {
+				for(int j = 0; j < 3; j++) {
+					temp[i][j] = this.state[j][2-i];
+				}
+			} 
+			this.state = temp;
+		}
+		
 		public char[] getRow(int rowNum) {
 			char[] row = new char[3];
 			for(int i = 0; i < 3; i++) {
